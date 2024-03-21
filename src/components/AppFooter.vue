5c4f63163f5b15d8270f5d6db102824e373a094e
chrialge <chrialge99@gmail.com>
2024-03-21 17:07:52 +0100

    add main

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,6 +9,39 @@ export default {
 }
 </script>
 <template>
-    <h3>{{ message }}</h3>
+        <!-- site-footer -->
+        <footer id="site-footer">
+        <div class="container d-flex footer-space">
+
+            <!-- credetials -->
+            <div class="credentials ">
+                <h2>Boolando s.r.l</h2>
+                <div class="policy">
+                    <ul>
+                        <li><a href="#">Informazioni legali</a></li>
+                        <li><a href="#">Informativa sulla privacy</a></li>
+                        <li><a href="#">Diritto di recesso</a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /credetials -->
+
+            <!-- social -->
+            <div class="social ">
+                <h3>Trovaci anche su</h3>
+                <div class="social-widget d-flex">
+                    <img src="./assets/img/widget/square-twitter.svg" alt="">
+                    <img src="./assets/img/widget/square-facebook.svg" alt="">
+                    <img src="./assets/img/widget/square-instagram.svg" alt="">
+                    <img src="./assets/img/widget/pinterest.svg" alt="">
+                    <img src="./assets/img/widget/square-youtube.svg" alt="">
+                </div>
+            </div>
+            <!-- /social -->
+            
+    
+        </div>
+    </footer>
+    <!-- /#site-footer -->
 </template>
 <style></style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,6 +9,172 @@ export default {
 }
 </script>
 <template>
-    <h3>{{ message }}</h3>
+    <!-- site main -->
+    <main id="site-main">
+
+        <div class="container">
+            <div class="row">
+
+                <!-- first product -->
+                <div class="product col-4">
+                    <div class="card" style="width:18rem;">
+                        <!-- image with badge -->
+                        <img src="/images/1.webp" class="card-img-top" alt="...">
+                        <div class="heart"> &#9829;</div>
+                        <div class="badges">
+                            <div class="sales inline-block-container"> -50%</div>
+                            <div class="sostenibility inline-block-container">Sostenibilita</div>
+                        </div>
+                        <!-- /image with badge -->
+
+                        <!-- card body -->
+                        <div class="card-body">
+                            <h6 class="card-subtitle mb-2 text-muted ">Levi's</h6>
+                            <h5 class="card-title">RELAXED FIT TEE UNISEX</h5>
+                            <div class="price">
+                                <div class="price">
+                                    <div class="price-at-the-moment">14,99 &euro;</div>
+                                    <div class="full-price"><s>29,99 &euro;</s></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /card body -->
+                    </div>
+                </div>
+
+                <!-- first product -->
+                <div class="product col-4">
+                    <div class="card" style="width:18rem;">
+                        <!-- image with badge -->
+                        <img src="/images/2.webp" class="card-img-top" alt="...">
+                        <div class="heart"> &#9829;</div>
+                        <div class="badges">
+                            <div class="sales inline-block-container"> -30%</div>
+                        </div>
+                        <!-- /image with badge -->
+
+                        <!-- card body -->
+                        <div class="card-body">
+                            <h6 class="card-subtitle mb-2 text-muted ">Guess</h6>
+                            <h5 class="card-title">ROSES TEE</h5>
+                            <div class="price">
+                                <div class="price">
+                                    <div class="price-at-the-moment">20,99 &euro;</div>
+                                    <div class="full-price"><s>29,99 &euro;</s></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /card body -->
+                    </div>
+                </div>
+
+                                <!-- first product -->
+                                <div class="product col-4">
+                    <div class="card" style="width:18rem;">
+                        <!-- image with badge -->
+                        <img src="/images/3.webp" class="card-img-top" alt="...">
+                        <div class="heart"> &#9829;</div>
+                        <div class="badges">
+                            <div class="sales inline-block-container"> -30%</div>
+                        </div>
+                        <!-- /image with badge -->
+
+                        <!-- card body -->
+                        <div class="card-body">
+                            <h6 class="card-subtitle mb-2 text-muted ">Come Zucchero Filato</h6>
+                            <h5 class="card-title">VOGLIA DI COLORI PASTELLO</h5>
+                            <div class="price">
+                                <div class="price">
+                                    <div class="price-at-the-moment">129,99 &euro;</div>
+                                    <div class="full-price"><s>184,99 &euro;</s></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /card body -->
+                    </div>
+                </div>
+
+                <!-- first product -->
+                <div class="product col-4">
+                    <div class="card" style="width:18rem;">
+                        <!-- image with badge -->
+                        <img src="/images/4.webp" class="card-img-top" alt="...">
+                        <div class="heart"> &#9829;</div>
+                        <div class="badges">
+                            <div class="sales inline-block-container"> -50%</div>
+                            <div class="sostenibility inline-block-container">Sostenibilita</div>
+                        </div>
+                        <!-- /image with badge -->
+
+                        <!-- card body -->
+                        <div class="card-body">
+                            <h6 class="card-subtitle mb-2 text-muted ">Levi's</h6>
+                            <h5 class="card-title">TEE UNISEX</h5>
+                            <div class="price">
+                                <div class="price">
+                                    <div class="price-at-the-moment">14,99 &euro;</div>
+                                    <div class="full-price"><s>29,99 &euro;</s></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /card body -->
+                    </div>
+                </div>
+
+                                <!-- first product -->
+                                <div class="product col-4">
+                    <div class="card" style="width:18rem;">
+                        <!-- image with badge -->
+                        <img src="/images/5.webp" class="card-img-top" alt="...">
+                        <div class="heart"> &#9829;</div>
+                        <!-- /image with badge -->
+
+                        <!-- card body -->
+                        <div class="card-body">
+                            <h6 class="card-subtitle mb-2 text-muted ">Maya Deluxe</h6>
+                            <h5 class="card-title">STRIPE BODICE</h5>
+                            <div class="price">
+                                <div class="price">
+                                    <div class="price-at-the-moment">99,99 &euro;</div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /card body -->
+                    </div>
+                </div>
+
+                <!-- first product -->
+                <div class="product col-4">
+                    <div class="card" style="width:18rem;">
+                        <!-- image with badge -->
+                        <img src="/images/6.webp" class="card-img-top" alt="...">
+                        <div class="heart"> &#9829;</div>
+                        <div class="badges">
+                            <div class="sostenibility inline-block-container">Sostenibilita</div>
+                        </div>
+                        <!-- /image with badge -->
+
+                        <!-- card body -->
+                        <div class="card-body">
+                            <h6 class="card-subtitle mb-2 text-muted ">Guess</h6>
+                            <h5 class="card-title">ROSES TEE</h5>
+                            <div class="price">
+                                <div class="price">
+                                    <div class="price-at-the-moment">29,99 &euro;</div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /card body -->
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+
+
+    </main>
+    <!-- /#site-main -->
 </template>
 <style></style>
