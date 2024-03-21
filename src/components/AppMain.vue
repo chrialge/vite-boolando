2757bd5579e118dbf60bd6dbc170cc299bd8fc4d
chrialge <chrialge99@gmail.com>
2024-03-21 18:01:01 +0100

    add array and fix height at a cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,7 +3,123 @@ export default {
     name: 'AppMain',
     data() {
         return {
-            message: 'orco'
+            products: [
+                {
+                    
+                    gener: "RELAXED FIT TEE UNISEX",
+                    brand: "Levi's",
+                    badges: 
+                    [{
+                       discount: '50%',
+                        sostenibility: true 
+                    }],
+                    price: 
+                    [{
+                      priceDiscount: 14.99,
+                        fullPrice: 29.99
+                    }],
+                    images: [{
+                        front: '1.webp',
+                        back: '1b.webp',   
+                    }],
+                },
+                {
+                    
+                    gener: "ROSES TEE",
+                    brand: "Guess",
+                    badges: 
+                    [{
+                       discount: '30%',
+                        sostenibility: false 
+                    }],
+                    price: 
+                    [{
+                      priceDiscount: 20.99,
+                        fullPrice: 29.99
+                    }],
+                    images: [{
+                        front: '2.webp',
+                        back: '2b.webp',   
+                    }],
+                },
+                {
+                    
+                    gener: "VOGLIA DI COLORI PASTELLO",
+                    brand: "Come Zucchero Filato",
+                    badges: 
+                    [{
+                       discount: '30%',
+                        sostenibility: false
+                    }],
+                    price: 
+                    [{
+                      priceDiscount: 129.99,
+                        fullPrice: 184.99
+                    }],
+                    images: [{
+                        front: '3.webp',
+                        back: '3b.webp',   
+                    }],
+                },
+                {
+                    
+                    gener: "TEE UNISEX",
+                    brand: "Levi's",
+                    badges: 
+                    [{
+                       discount: '50%',
+                        sostenibility: true 
+                    }],
+                    price: 
+                    [{
+                      priceDiscount: 14.99,
+                        fullPrice: 29.99
+                    }],
+                    images: [{
+                        front: '4.webp',
+                        back: '4b.webp',   
+                    }],
+                },
+                {
+                    
+                    gener: "STRIPE BODICE",
+                    brand: "Maya Deluxe",
+                    badges: 
+                    [{
+                       discount: false,
+                        sostenibility: true 
+                    }],
+                    price: 
+                    [{
+                      priceDiscount: 14.99,
+                        fullPrice: false
+                    }],
+                    images: [{
+                        front: '5.webp',
+                        back: '5b.webp',   
+                    }],
+                },
+                {
+                    
+                    gener: "ROSES TEE",
+                    brand: "Guess",
+                    badges: 
+                    [{
+                       discount: false,
+                        sostenibility: true 
+                    }],
+                    price: 
+                    [{
+                      priceDiscount: 14.99,
+                        fullPrice: false
+                    }],
+                    images: [{
+                        front: '6.webp',
+                        back: '6b.webp',   
+                    }],
+                },
+
+            ]
         }
     }
 }
