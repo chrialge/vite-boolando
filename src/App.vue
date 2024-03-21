6a5c1fff87633fcffb95a5d65290a7572f600b5b
chrialge <chrialge99@gmail.com>
2024-03-21 15:13:27 +0100

    add repo

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: 'App',
+  data() {
+    return{
+      message: 'ciao'  
+    }
+    
+  }
+}
+</script>
+
+<template>
+  <h1>{{message}}</h1>
+</template>
+
+<style></style>
